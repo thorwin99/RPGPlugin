8c77bf8930239326d7c7428d2cc5ce025c0da26b
BuildTools <unconfigured@null.spigotmc.org>
2017-08-10 16:50:34 +0200

    Added restricted interactable Items

--- src/com/mcoldlife/listeners/playerInteractEvent.java
+++ src/com/mcoldlife/listeners/playerInteractEvent.java
@@ -38,6 +38,7 @@ public class playerInteractEvent implements Listener{
 		runItemEvents(item, e);//All Item interact events which aren't bound to restrictions go here
 
 		//TODO Cancel only if restricted intractable
+		if(!RPGManager.restrictedInteractItems.contains(clickedBlock.getType()))return;
 		if(e.getAction() != Action.RIGHT_CLICK_BLOCK)return;
 		if(player.getLand() == null){//TODO: is not important. only for name...
 			e.setCancelled(true);
